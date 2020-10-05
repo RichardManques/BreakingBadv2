730f1859772de89cf8305103ccceabeed4214a66
RichardManques <69742054+RichardManques@users.noreply.github.com>
2020-10-04 21:27:15 -0300

    Revert "Splash Screen Creation"

--- app/src/main/java/com/example/breakingbadprueba/adapter/AdapterCharacter.java
+++ app/src/main/java/com/example/breakingbadprueba/adapter/AdapterCharacter.java
@@ -6,12 +6,15 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
 import com.bumptech.glide.Glide;
 import com.example.breakingbadprueba.MainActivity;
 import com.example.breakingbadprueba.R;
 import com.example.breakingbadprueba.model.Character;
+
 import java.util.List;
 
 public class AdapterCharacter extends RecyclerView.Adapter<AdapterCharacter.CharacterHolder> {
