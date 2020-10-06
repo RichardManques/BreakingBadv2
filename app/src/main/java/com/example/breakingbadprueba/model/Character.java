704c77f07dc4530a04858cd0dd2208e2709d7641
LuxCaro19 <luiscaroretamal2000@gmail.com>
2020-10-06 18:44:53 -0300

    Seccion detalle de personaje terminada.

--- app/src/main/java/com/example/breakingbadprueba/model/Character.java
+++ app/src/main/java/com/example/breakingbadprueba/model/Character.java
@@ -2,15 +2,26 @@ package com.example.breakingbadprueba.model;
 
 public class Character {
     public String name;
+    public String apodo;
     public String img;
+    public String fecNac;
+    public String estado;
+    public String ocupacion;
+    public String serie;
+
 
     public Character(){
 
     }
 
-    public Character(String name, String img) {
+    public Character(String name, String apodo, String img, String fecNac, String estado, String ocupacion, String serie) {
         this.name = name;
+        this.apodo = apodo;
         this.img = img;
+        this.fecNac = fecNac;
+        this.estado = estado;
+        this.ocupacion = ocupacion;
+        this.serie = serie;
     }
 }
 
